a71494f00aa8c267f086b8d45a32994d93f6d11c
Lienid <0xLienid@protonmail.com>
2021-11-30 09:06:47 -0500

    Made it prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { Grid } from "@material-ui/core";
 import { projects } from "./projects";
 import { Chart } from "./components/Chart";
+import "./App.css";
 
 export const App = () => {
 
   return (
-    <div className="App">
-      {projects.map(project => {
-        return (
-          <Chart project={project} />
-        );
-      })}
+    <div className="App" style={{ backgroundColor: "#3D4875" }}>
+      <div className="charts-body">
+        <h2 className="dash-title">NPM Downloads Dashboard</h2>
+        <Grid container spacing={2}>
+          {projects.map(project => {
+            return (
+              <Chart project={project} />
+            );
+          })}
+        </Grid>
+      </div>
     </div>
   );
 }
--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
+import "./chart.css";
 
 import { getProjectData } from "../helpers/getProjectData";
 import { weeklyFromDaily } from "../helpers/downloadAgg";
 import { LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
+import { Grid } from "@material-ui/core";
 import { DownloadCSV } from "./DownloadCSV";
 
 export const Chart = (project) => {
@@ -11,37 +13,44 @@ export const Chart = (project) => {
   useEffect(() => {
     (async () => {
       let currentDate = new Date().toISOString().slice(0, 10);
-      const data = await getProjectData(project.project, "2010-01-01", currentDate);
+      const data = await getProjectData(project.project, "2014-01-01", currentDate);
       const weeklyData = weeklyFromDaily(data);
       setProjectData(weeklyData);
     })()
   }, []);
 
   return (
-    <div className="chart">
-      <div className="chart-head">
-        <h6>{project.project}</h6>
-        <DownloadCSV data={projectData} project={project.project} />
+    <Grid item xs={6}>
+      <div className="chart">
+        <div className="chart-head">
+          <h2 className="title">{project.project}</h2>
+          <DownloadCSV data={projectData} project={project.project} />
+        </div>
+        <LineChart
+          width={500}
+          height={500}
+          data={projectData}
+        >
+          <Line
+            type="monotone"
+            dataKey="downloads"
+            stroke="#C49662"
+            strokeWidth={3}
+            dot={false}
+            activeDot={{ r: 8 }}
+          />
+          <XAxis
+            dataKey="day"
+            tick={{ fill: "#B6B8C0" }}
+            stroke="#B6B8C0"
+          />
+          <YAxis
+            tick={{ fill: "#B6B8C0" }}
+            stroke="#B6B8C0"
+          />
+          <Tooltip />
+        </LineChart>
       </div>
-      <LineChart
-        width={500}
-        height={500}
-        data={projectData}
-      >
-        <Line
-          type="monotone"
-          dataKey="downloads"
-          stroke="#8884d8"
-          strokeWidth={3}
-          dot={false}
-          activeDot={{ r: 8 }}
-        />
-        <XAxis
-          dataKey="day"
-        />
-        <YAxis />
-        <Tooltip />
-      </LineChart>
-    </div>
+    </Grid>
   );
 }
