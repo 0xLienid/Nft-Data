7fa580e2e0eb1ada60628b1af4f4002ac1671845
Lienid <0xLienid@protonmail.com>
2021-12-01 23:12:34 -0500

    Pls

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,15 @@ export const App = () => {
 
   return (
     <div className="App" style={{ backgroundColor: "#3D4875" }}>
-      <Router>
+      <Router basename="/#/">
         <Routes>
-          <Route exact path="/Nft-Data" element={<AllNfts projectsData={projectsData} />} />
-          <Route exact path="/Nft-Data/ethereum" element={<NftsByCategory projectsData={projectsData} category="ethereum" />} />
-          <Route exact path="/Nft-Data/solana" element={<NftsByCategory projectsData={projectsData} category="solana" />} />
-          <Route exact path="/Nft-Data/binance-smart-chain" element={<NftsByCategory projectsData={projectsData} category="binance-smart-chain" />} />
-          <Route exact path="/Nft-Data/avalanche" element={<NftsByCategory projectsData={projectsData} category="avalanche" />} />
-          <Route exact path="/Nft-Data/polygon" element={<NftsByCategory projectsData={projectsData} category="polygon" />} />
-          <Route exact path="/Nft-Data/tezos" element={<NftsByCategory projectsData={projectsData} category="tezos" />} />
+          <Route exact path="/" element={<AllNfts projectsData={projectsData} />} />
+          <Route exact path="/ethereum" element={<NftsByCategory projectsData={projectsData} category="ethereum" />} />
+          <Route exact path="/solana" element={<NftsByCategory projectsData={projectsData} category="solana" />} />
+          <Route exact path="/binance-smart-chain" element={<NftsByCategory projectsData={projectsData} category="binance-smart-chain" />} />
+          <Route exact path="/avalanche" element={<NftsByCategory projectsData={projectsData} category="avalanche" />} />
+          <Route exact path="/polygon" element={<NftsByCategory projectsData={projectsData} category="polygon" />} />
+          <Route exact path="/tezos" element={<NftsByCategory projectsData={projectsData} category="tezos" />} />
         </Routes>
       </Router>
     </div>
